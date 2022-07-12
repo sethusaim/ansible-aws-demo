f4de92b45dff433534878b7a11d21449e0066833
sethusaim <sethusaim@gmail.com>
2022-07-12 12:39:53 +0530

    added module.tf

---
+++ infrastructure/module.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state-sethu"
+    key    = "tf_state"
+    region = "us-east-1"
+  }
+}
+
+module "ansible_ec2" {
+  source = "./app_ec2"
+}
+
+module "target_ec2" {
+  source = "./target_ec2"
+}
