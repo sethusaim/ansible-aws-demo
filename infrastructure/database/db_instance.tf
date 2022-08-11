7f9e37a64b56e67fad575177859bac48e53e1940
sethusaim <sethusaim@gmail.com>
2022-08-11 18:45:58 +0530

    minor fix

--- infrastructure/database/db_instance.tf
+++ infrastructure/database/db_instance.tf
@@ -9,6 +9,6 @@ resource "aws_db_instance" "mysql" {
   password               = var.mysql_password
   parameter_group_name   = var.parameter_group_name
   skip_final_snapshot    = var.skip_final_snapshot
-  vpc_security_group_ids = [aws_security_group.security_group.id]
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]
   publicly_accessible    = var.publicly_accessible
 }
